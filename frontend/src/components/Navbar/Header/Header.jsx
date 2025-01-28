0f473eefe4922b2e9a65bfa0b1a936b19f47693c
mercysound <adekunleadewolemercy@gmail.com>
2025-01-28 17:19:45 +0100

    first commit

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+         <h2>Order your favorite food here</h2>
+         <p>choose from a diverse menu featuring a delectable array of dishers crafted with the finest ingredients and satisfy your cravings and eleavate your dining experience, one delicious meal at a time.</p>
+         <button>View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
