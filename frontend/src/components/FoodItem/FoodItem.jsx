e283961edbae53e306e72cadda8db625b88bb51b
adewoleadekunlemercy <adekunleadewolemercy@gmail.com>
2026-01-20 15:51:44 +0100

    gitignore

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,10 +5,11 @@ import { StoreContext } from '../../context/FoodContext'
 const FoodItem = ({ id, name, price, description, image }) => {
 
   // const [itemCount, setItemCount] = useState(0)
+  // make difference
   const {cartItems, addToCart, removeFromCart, url} = useContext(StoreContext)
 
   return (
-    <div className='food-item'>
+    <div className='food-item'> 
       <div className="food-item-img-container">
         <img className='food-item-image' src={url+"/images/"+image} alt="" />
         {!cartItems[id]
